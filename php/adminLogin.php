214db6d4e9bf1279005b86c29080917b05408b3b
vidinh1 <118785324+vidinh1@users.noreply.github.com>
2023-11-30 21:24:39 -0500

    Added login forms
    Redirects to place holder pages

---
+++ php/adminLogin.php
@@ -0,0 +1,38 @@
+<?php
+
+    $username = htmlspecialchars($_POST['username']);
+    $password = htmlspecialchars($_POST['password']);
+
+
+    $mysqli = mysqli_connect("localhost", "adminuser", "admin", "bookstoredb");
+
+    //check connection
+    if(mysqli_connect_error()){
+        echo "Failed to connect to maria Db" . $mysqli->connect_error;
+
+        die("error coenncting");
+    }
+
+    //first check if admin
+    //then check if user exist
+
+    //insert statement
+
+
+    $sql = "SELECT *FROM accounts WHERE Username = '{$username}' AND Password = '{$password}' AND Type = '0';";
+
+    $result = mysqli_query($mysqli, $sql);
+
+    if (mysqli_num_rows($result) === 1) {
+        header("Location: http://localhost/CIS_435_Project_4/admin/admin.html");
+    }
+
+
+    else{
+        echo("Invalid");
+    }
+
+    $mysqli->close();
+
+    
+?>
