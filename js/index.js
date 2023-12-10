b13173823cb41e5e9b4a48a973589ae32d1c9db8
samcam004 <samcam@umich.edu>
2023-12-10 13:39:20 -0500

    Pop Up Fix
    Fixed input fields to fit within the pop up form.
    
    Fixed the button for Create Account.

--- js/index.js
+++ js/index.js
@@ -8,9 +8,11 @@ function start() {
     getButton.onclick = getButtonClick;
 }
 
-//LOGIN Type 1 start
+//Opens pop up menu for User login, calls closing function for all other pop up menus
 async function openUser() {
   document.getElementById("userForm").style.display = "block";
+  console.log("Open User");
+
   if (document.getElementById("adminForm").style.display = "block") {
     document.getElementById("adminForm").style.display = "none";
   }
@@ -19,12 +21,17 @@ async function openUser() {
   }
 }
 
+//Closes pop up menu for User login
 async function closeUser() {
   document.getElementById("userForm").style.display = "none";
+  console.log("Close User");
 }
 
+//Opens pop up menu for Admin login, calls closing function for all other pop up menus
 async function openAdmin() {
   document.getElementById("adminForm").style.display = "block";
+  console.log("Open Admin");
+
   if (document.getElementById("userForm").style.display = "block") {
     document.getElementById("userForm").style.display = "none";
   }
@@ -33,12 +40,17 @@ async function openAdmin() {
   }
 }
 
+//Closes pop up menu for Admin login
 async function closeAdmin() {
     document.getElementById("adminForm").style.display = "none";
+    console.log("Close Admin");
 }
 
+//Opens pop up menu for Create User, calls closing function for all other pop up menus
 async function openCreate() {
   document.getElementById("createForm").style.display = "block";
+  console.log("Open Create");
+
   if (document.getElementById("userForm").style.display = "block") {
     document.getElementById("userForm").style.display = "none";
   }
@@ -47,10 +59,11 @@ async function openCreate() {
   }
 }
 
+//Closes pop up menu for Create User
 async function closeCreate() {
   document.getElementById("createForm").style.display = "none";
+  console.log("Close Create");
 }
-//LOGIN Type 1 end
 
 //create button, passes html data to php
 async function createButtonClick(){
@@ -96,7 +109,6 @@ async function getButtonClick(){
 
 //use POST to send data to php
 async function create(username,password,email) {
-    
     try {
         await fetch(
             'http://localhost/CIS_435_Project_4/php/createAccount.php', 
