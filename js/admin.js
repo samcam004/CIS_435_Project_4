3f07217716ea267c104710147eb8e63e512c1cc9
Jubury <99433261+Jubury@users.noreply.github.com>
2023-12-10 18:27:05 -0500

    Add deleted functionality to Admin

--- js/admin.js
+++ js/admin.js
@@ -1,38 +1,10 @@
 function start() {
     fetchBooks();
+    fetchISBNs();
     const addButton = document.getElementById('btn-submit-data');
     addButton.addEventListener('click', addBook);
     const deleteButton = document.getElementById('btn-delete-data');
     deleteButton.addEventListener('click', deleteBook);
-}
-
-async function addBook(){
-    // Grabbing data for new book
-    const newBook = {
-        "isbn": document.getElementById('ISBN').value,
-        "name": document.getElementById('name').value,
-        "author": document.getElementById('author').value,
-        "genre": document.getElementById('genre').value,
-        "price": parseFloat(document.getElementById('price').value)
-    }
-    // Log the newBook to see if you're capturing form data correctly.
-    console.log(newBook);
-    // Sending data back to API
-    try {
-        const response = await fetch('http://localhost/projects/CIS_435_Project_4/php/createBook.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(newBook)
-        });
-
-        const responseText = await response.text();
-        console.log(responseText);
-        
-    } catch (error) {
-        console.error('Error:', error.message);
-    }
 }
 
 async function fetchBooks() {
@@ -78,8 +50,74 @@ async function fetchBooks() {
     }
 }
 
-async function deleteBook(){
+async function fetchISBNs() {
+    try {
+        const response = await fetch('http://localhost/projects/CIS_435_Project_4/php/readBooks.php');
+        const json = await response.json();
+        const deleteDropdown = document.getElementById('delete_id');
 
+        // Clearing any previous dropdown options
+        deleteDropdown.innerHTML = '';
+
+        // Create an option for each ISBN found in the DB
+        json.forEach(book => {
+            const option = document.createElement('option');
+            option.value = book.ISBN;
+            option.textContent = book.ISBN;
+            deleteDropdown.appendChild(option);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function addBook(){
+    // Grabbing data for new book
+    const newBook = {
+        "isbn": document.getElementById('ISBN').value,
+        "name": document.getElementById('name').value,
+        "author": document.getElementById('author').value,
+        "genre": document.getElementById('genre').value,
+        "price": parseFloat(document.getElementById('price').value)
+    }
+    // Log the newBook to see if you're capturing form data correctly.
+    console.log(newBook);
+    // Sending data back to API
+    try {
+        const response = await fetch('http://localhost/projects/CIS_435_Project_4/php/createBook.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newBook)
+        });
+
+        const responseText = await response.text();
+        console.log(responseText);
+        
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function deleteBook() {
+    const selectedISBN = document.getElementById('delete_id').value;
+    try {
+        const response = await fetch('http://localhost/projects/CIS_435_Project_4/php/deleteBook.php', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ ISBN: selectedISBN })
+        });
+        const responseText = await response.text();
+        console.log(responseText);
+        // After deleting the book, refresh the page 
+        fetchISBNs();
+        fetchBooks();
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 window.addEventListener('load', start);
